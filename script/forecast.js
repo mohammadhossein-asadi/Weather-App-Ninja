0006be1b93aa67c13dab02c75369d8e60239198a
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-14 19:44:12 +0330

    commit

--- script/forecast.js
+++ script/forecast.js
@@ -2,7 +2,14 @@ const key = "IVARrjZAiQb4xvifjwuaH34rAS0fGxjc";
 
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
-  const query = `?apiKey=$${key}$q=${city}`;
+
+  const query = `?apikey=${key}&q=${city}`;
+
 
   const response = await fetch(base + query);
+  const data = await response.json();
+
+  console.log(data);
 };
+
+getCity("manchester");
