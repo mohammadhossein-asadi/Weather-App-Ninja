f5a8582b3f6f826009426315ad61caf2e01bce72
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-14 19:32:58 +0330

    change

--- script/forecast.js
+++ script/forecast.js
@@ -1,1 +1,8 @@
 const key = "IVARrjZAiQb4xvifjwuaH34rAS0fGxjc";
+
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apiKey=$${key}$q=${city}`;
+
+  const response = await fetch(base + query);
+};
