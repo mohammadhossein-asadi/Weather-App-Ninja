c0c86a274a4c88f060d3d89f100176d9f780d64f
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-16 20:44:57 +0330

    commit

--- script/app.js
+++ script/app.js
@@ -2,11 +2,20 @@ const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
 
+const updateUI = (data) => {
+  const cityDets = data.cityDets;
+  const weather = data.weather;
 
-const updateUI = ()=>{
-    
-}
-
+  //   update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+          <div class="my=3">${weather.WeatherText}</div>
+          <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+          </div>
+`;
+};
 
 const updateCity = async (city) => {
   const cityDets = await getCity(city);
